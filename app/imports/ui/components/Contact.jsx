bd920753a8389deb8584ed386b8ad2d446344b32
jhasun <jhasun@hawaii.edu>
2024-03-28 17:47:08 -1000

    notes-1 finished in 26 minutes.

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image } from 'react-bootstrap';
+import { Card, Image, ListGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Note from './Note';
+import AddNote from './AddNote';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const Contacts = ({ contact }) => (
+const Contacts = ({ contact, notes }) => (
   <Card className="h-100">
     <Card.Header>
       <Image src={contact.image} width={75} />
@@ -13,6 +15,10 @@ const Contacts = ({ contact }) => (
     </Card.Header>
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
+      <ListGroup variant="flush">
+        {notes.map((note) => <Note key={note._id} note={note} />)}
+      </ListGroup>
+      <AddNote owner={contact.owner} contactId={contact._id} />
       <Link to={`/edit/${contact._id}`}>Edit</Link>
     </Card.Body>
   </Card>
@@ -27,7 +33,15 @@ Contacts.propTypes = {
     image: PropTypes.string,
     description: PropTypes.string,
     _id: PropTypes.string,
+    owner: PropTypes.string,
   }).isRequired,
+  notes: PropTypes.arrayOf(PropTypes.shape({
+    note: String,
+    contactId: String,
+    createdAt: PropTypes.instanceOf(Date),
+    owner: String,
+    _id: PropTypes.string,
+  })).isRequired,
 };
 
 export default Contacts;
